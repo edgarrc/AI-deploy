bd9213af1f548a723f72302a62e6f969070f728b
Edgar <edgarrc@users.noreply.github.com>
2019-07-15 14:32:48 -0300

    Update run.py

--- sampleapp/run.py
+++ sampleapp/run.py
@@ -1,13 +1,22 @@
 from wsgiref.simple_server import make_server
 from pyramid.config import Configurator
 from pyramid.response import Response
+import sys
 
 if __name__ == '__main__':
+
+    # Print every command line parameter received
+    print("Parameters: " + str(len(sys.argv)) + " values: " + str(sys.argv))	
     
-    print("Iniciando servico em " +  str(sys.argv[0]))
+    # Default port to listen
+    port = 8080
+    if len(sys.argv) > 1:
+        port = int(sys.argv[1])
+		
     
     config = Configurator()
 	
+    print("Starting the service " + str(port))    
     app = config.make_wsgi_app()
-    server = make_server('0.0.0.0', sys.argv[0], app)
-    server.serve_forever()		
+    server = make_server('0.0.0.0', port, app)
+    server.serve_forever()	
